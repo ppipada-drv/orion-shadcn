a6098027fe27992f8a2d582b9d8fd88780f482dc
Pankaj Pipada <pankaj.pipada@druva.com>
2025-05-22 20:02:27 +0530

    build cleanup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+	stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(ts|tsx)'],
 	addons: ['@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
 	framework: {
 		name: '@storybook/react-vite',
